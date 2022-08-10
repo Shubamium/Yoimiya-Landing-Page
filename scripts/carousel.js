0eef3393c863ecbdfcf3f588002fe0337c8dcb69
Vincent Liuss <liusvinv@gmail.com>
2022-08-11 06:08:58 +0700

    Add a carousel functionality

---
+++ scripts/carousel.js
@@ -0,0 +1,110 @@
+document.addEventListener('DOMContentLoaded',initCarousel);
+
+var carouselCurrentPageIds = [];
+
+//Initialize every carousel
+function initCarousel(){
+
+    //Get Every Carousels in the DOM
+    let carousels = document.getElementsByClassName('carousel');
+
+    //Loop through each carousel and initialize it
+    for(let i = 0; i < carousels.length; i++){
+        carouselStart(carousels[i] , i);    
+        carouselCurrentPageIds[i] = 0;
+    }
+}
+
+//Initialize a single carousel
+function carouselStart(carousel, carouselId){
+    
+    //Get All of the carousels-items within the carousel
+    let carouselItems = carousel.getElementsByClassName('carousel-item');
+
+    //Get the element for the controls
+    let carouselControls = carousel.getElementsByClassName('carousel-controls')[0];
+    let prevButton = carouselControls.getElementsByClassName('prev')[0];
+    let nextButton = carouselControls.getElementsByClassName('next')[0];
+    let pageIndicator = carouselControls.getElementsByClassName('indicator')[0];
+
+   // setTheActivePage(carouselItems,toSetActive);
+    
+   
+
+    //Set the controls function
+    prevButton.addEventListener('click',() =>{
+        //Decrement the carousels Current page id
+        let targetPageId = carouselCurrentPageIds[carouselId];
+        targetPageId = unaryLimiter(false,targetPageId,0,carouselItems.length-1);
+        carouselCurrentPageIds[carouselId] = targetPageId;
+        
+        //Set the current page
+        setTheActivePage(carouselItems, targetPageId);
+        
+        //Set the page number
+        if(pageIndicator != null) pageIndicator.textContent = ++targetPageId;
+
+
+        console.log(carouselCurrentPageIds[0]);
+
+    });
+
+    nextButton.addEventListener('click',() =>{
+        //Increment the carousels Current page id
+        let targetPageId = carouselCurrentPageIds[carouselId];
+        targetPageId = unaryLimiter(true,targetPageId,0,carouselItems.length-1);
+        carouselCurrentPageIds[carouselId] = targetPageId;
+        
+        //Set the current page
+        setTheActivePage(carouselItems, targetPageId);
+
+        //Set the page number
+        if(pageIndicator != null) pageIndicator.textContent = ++targetPageId;
+
+
+        console.log(carouselCurrentPageIds[0]);
+    });
+
+
+
+}
+
+
+
+//Add a min and max for the unary operators
+//--isIncrement = Do you want the number to be incremented?
+function unaryLimiter(isIncrement, numbers, min, max){
+
+    if(isIncrement){
+        //Increment the number
+        if(numbers >= max){
+            numbers = min;
+        }else{
+            numbers++;
+        }
+
+    }else{
+        //Decrement the numbers
+        if(numbers <= min){
+            numbers = max;
+        }else{
+            numbers--;
+        }
+        
+    }
+    return numbers;
+}
+
+//Hide every other item in the array that isn't the given parameter
+function setTheActivePage(itemsList, activeIndex){
+
+    //Iterate every item and hide it
+    for(let i = 0; i < itemsList.length;i++){
+        itemsList[i].classList.remove('hidden');
+        itemsList[i].classList.add('hidden');
+    }
+
+    //Enable the element that is the activeIndex
+    itemsList[activeIndex].classList.remove('hidden');
+
+}
